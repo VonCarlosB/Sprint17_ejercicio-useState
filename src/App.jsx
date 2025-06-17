90d16bc9bb00478de41efc6f1980d6d4433feafc
CarlosBF <cborrellf@outlook.es>
2025-06-17 20:53:00 +0200

    Implemented bonus

--- src/App.jsx
+++ src/App.jsx
@@ -3,26 +3,31 @@ import './App.css'
 
 function App() {
     const [name, setName] = useState('Sofía')
-    // const [newName, setNewName] = useState('')
+    const [newName, setNewName] = useState('')
 
-    const changeName = nombre => {
-        setName(nombre)
+    const changeName = () => {
+        if(newName.trim() !== ''){
+            setName(newName)
+        }
+        setNewName('')
     }
 
-    // const changeNameTo = nombre => {
-    //     setNewName(nombre)
-    //     changeName()
-    // }
-
     return(
-        <div>
+        <>
+            <form onSubmit={(e) => {
+                    e.preventDefault()
+                    changeName()
+                }}>
+                <input type='text' value={newName} placeholder='Pon el nombre del profe' onChange={(e) => setNewName(e.target.value)} />
+                <button type='submit'>Nuevo profesor</button>
+            </form>
             <h2>Teacher name: {name}</h2>
             <ul>
-                <li onClick={() => changeName('Data')}>Data</li>
-                <li onClick={() => changeName('Reyes')}>Reyes</li>
-                <li onClick={() => changeName('Celia')}>Celia</li>
+                <li onClick={() => setName('Data')}>Data</li>
+                <li onClick={() => setName('Reyes')}>Reyes</li>
+                <li onClick={() => setName('Celia')}>Celia</li>
             </ul>
-        </div>
+        </>
     )
 }
 
